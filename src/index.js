d72d885e1554f2a5b06e8be0d3bde0ae2a00e385
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2022-08-15 21:33:13 +0300

    add slider, style section testimonials

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import './scss/index.scss'
 import { makeAlert } from './js/Front-page'
 
 // makeAlert("HI!!!!")
+// $(document).ready(function () {
+//   $('.testimonial-slider').slick();
+// });
--- src/js/Front-page.js
+++ src/js/Front-page.js
@@ -1,4 +1,13 @@
 
-// export function makeAlert(text) {
-//     alert(text)
-// }
+import Splide from '@splidejs/splide';
+import '@splidejs/splide/css';
+import '@splidejs/splide/css/skyblue';
+import '@splidejs/splide/css/sea-green';
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  new Splide('.splide', {
+    type: 'loop',
+    heightRatio: 0.5,
+  }).mount();
+});
