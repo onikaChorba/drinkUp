967278c31463c9021cfc285f3bf9fa3920fcaf97
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2022-08-16 09:33:44 +0300

    slyle slider and style section testimonial

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
 import './scss/index.scss'
 import { makeAlert } from './js/Front-page'
 
-// makeAlert("HI!!!!")
-// $(document).ready(function () {
-//   $('.testimonial-slider').slick();
-// });
--- src/js/Front-page.js
+++ src/js/Front-page.js
@@ -7,7 +7,8 @@ import '@splidejs/splide/css/sea-green';
 
 document.addEventListener('DOMContentLoaded', function () {
   new Splide('.splide', {
-    type: 'loop',
+    type: 'slider',
     heightRatio: 0.5,
+    "perPage": 1,
   }).mount();
 });
