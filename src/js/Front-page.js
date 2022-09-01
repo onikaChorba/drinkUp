8ef551ad42908b5603d4f58aba82828f1ca06848
Onika Chorba <onikachorba@MacBook-Pro-Dream-2.local>
2022-09-01 14:18:46 +0300

    move img in circle

--- src/js/Front-page.js
+++ src/js/Front-page.js
@@ -10,38 +10,54 @@ document.addEventListener('DOMContentLoaded', function () {
     type: 'slider',
     heightRatio: 0.5,
     "perPage": 1,
-  }).mount();
-  navSlide();
+  }).mount(),
+    navSlide()
+  imgAnimate()
 });
 
 //animation image
-// console.clear();
-// let angle = 360 / 6;
-// let finalAngle = -360;
-// let scale = 3 / 6;
-// let finalScale = -3;
 
-// const imageContainer = document.querySelectorAll(".image");
-// imageContainer.forEach(image => {
+function imgAnimate() {
+  const images = document.querySelectorAll(".image");
+  let zeroTop = 0;
+  let zeroLeft = 0;
+  let zeroTransform = "";
+  for (let i = 0; i < images.length; i++) {
+    let currentElement = images[i];
+    let nextElement = images[i + 1];
+    if (i === 0) {
+      zeroTop = window
+        .getComputedStyle(currentElement, null)
+        .getPropertyValue("top");
+      zeroLeft = window
+        .getComputedStyle(currentElement, null)
+        .getPropertyValue("left");
+      zeroTransform = window
+        .getComputedStyle(currentElement, null)
+        .getPropertyValue("transform");
+    }
+    if (nextElement) {
+      let nextTop = window
+        .getComputedStyle(nextElement, null)
+        .getPropertyValue("top");
+      let nextLeft = window
+        .getComputedStyle(nextElement, null)
+        .getPropertyValue("left");
+      let nextTransform = window
+        .getComputedStyle(nextElement, null)
+        .getPropertyValue("transform");
 
-//   finalAngle -= 60;
-//   image.style.transform = `rotate(${angle}deg)`;
-//   image.style.setProperty("--start-angle-of-rotation", `-${angle}deg`);
-//   image.style.setProperty("--end-angle-of-rotation", `${finalAngle}deg`);
-//   angle += 60;
-
-// });
-// imageContainer.forEach(image => {
-
-//   finalScale = 3;
-//   image.style.transform = `scale(${scale})`;
-//   image.style.setProperty(`-${scale}`);
-//   image.style.setProperty(`${finalScale}`);
-//   scale += 0.5;
-
-// });
-
-
+      currentElement.style.top = nextTop;
+      currentElement.style.left = nextLeft;
+      currentElement.style.transform = nextTransform;
+    } else {
+      currentElement.style.top = zeroTop;
+      currentElement.style.left = zeroLeft;
+      currentElement.style.transform = zeroTransform;
+    }
+  }
+}
+setInterval(() => imgAnimate(), 3000);
 
 
 //menu
